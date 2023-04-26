ec580fe7fbe3170f6edd21c74163c50a30a98127
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-26 10:21:23 +0530

    error resolved

--- arr2.java
+++ arr2.java
@@ -1,31 +1,31 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class arr2 {
-
     public static void main(String[] args) {
         int[] arr = {5, 3, 1, 8, 6};
-        int[] sortedArr = sortByBits(arr);
-        System.out.println(Arrays.toString(sortedArr));
+        sortByBits(arr);
+//        System.out.println(Arrays.toString(sortedArr));
     }
 
-    public static int[] sortByBits(int[] arr) {
-       Arrays.sort(arr);
+    public static void sortByBits(int[] arr) {
+//        Arrays.sort(arr);
 
-        for (int i = 1; i < arr.length; i++) {
-            int key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && Integer.bitCount(arr[j]) > Integer.bitCount(key)) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
 
-            while (j >= 0 && Integer.bitCount(arr[j]) == Integer.bitCount(key) && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+                if (Integer.bitCount(arr[i]) > Integer.bitCount(arr[j])) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                } else if (Integer.bitCount(arr[i]) == Integer.bitCount(arr[j])) {
+                    if (arr[i] > arr[j]) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
             }
-            arr[j + 1] = key;
         }
-        return arr;
+        System.out.println(Arrays.toString(arr));
     }
 }
